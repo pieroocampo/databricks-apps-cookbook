57a3a9d4cf4dece0557faab4423d90355dedf5cd
Pascal <mail@pascal-vogel.com>
2025-03-11 20:26:03 +0100

    Landing page

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -13,29 +13,29 @@ function HomepageHeader() {
   return (
     <div className="relative flex h-full w-full flex-1 items-center justify-center overflow-hidden">
       <HeaderAnimation isDarkMode={isDarkTheme} />
-      <div className="absolute z-10 flex w-full flex-col items-center justify-center px-4 text-center text-white md:px-6">
+      <div className="absolute z-10 flex w-full flex-col items-center justify-center px-6 text-center text-white">
         <Heading
           as="h1"
-          className="text-3xl font-bold text-gray-800 md:text-7xl dark:text-white"
+          className="text-7xl font-bold text-gray-800 dark:text-white"
         >
           {siteConfig.title}
         </Heading>
-        <p className="mt-3 max-w-3xl text-lg leading-relaxed text-gray-800 md:mt-4 md:text-3xl md:leading-10 dark:text-white">
+        <p className="mt-4 max-w-3xl text-3xl leading-10 text-gray-800 dark:text-white">
           {siteConfig.tagline}
         </p>
-        <div className="mt-6 flex flex-col items-center justify-center space-y-3 md:mt-4 md:flex-row md:space-y-0 md:space-x-8">
+        <div className="mt-4 flex flex-wrap items-center justify-center space-x-8 align-middle">
           <Link to="/docs/category/streamlit">
-            <button className="w-full border-2 border-lava-600 bg-lava-600 px-6 py-2.5 align-middle text-base font-semibold text-white hover:cursor-pointer hover:border-lava-700 hover:bg-lava-700 hover:underline md:w-auto md:px-8 md:text-lg">
+            <button className="border-2 border-lava-600 bg-lava-600 px-8 py-2.5 align-middle text-lg font-semibold text-white hover:cursor-pointer hover:border-lava-700 hover:bg-lava-700 hover:underline">
               Streamlit
             </button>
           </Link>
           <Link to="/docs/category/dash">
-            <button className="w-full border-2 border-lava-600 bg-lava-600 px-6 py-2.5 align-middle text-base font-semibold text-white hover:cursor-pointer hover:border-lava-700 hover:bg-lava-700 hover:underline md:w-auto md:px-8 md:text-lg">
+            <button className="border-2 border-lava-600 bg-lava-600 px-8 py-2.5 align-middle text-lg font-semibold text-white hover:cursor-pointer hover:border-lava-700 hover:bg-lava-700 hover:underline">
               Dash
             </button>
           </Link>
           <Link to="/docs/intro">
-            <button className="w-full border-2 border-gray-800 bg-transparent px-6 py-2.5 align-middle text-base font-semibold text-gray-800 hover:cursor-pointer hover:underline md:w-auto md:px-8 md:text-lg dark:border-white dark:text-white">
+            <button className="border-2 border-gray-800 bg-transparent px-8 py-2.5 align-middle text-lg font-semibold text-gray-800 hover:cursor-pointer hover:underline dark:border-white dark:text-white">
               Learn more
             </button>
           </Link>
