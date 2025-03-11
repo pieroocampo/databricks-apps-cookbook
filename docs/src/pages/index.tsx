400f25e9b38bb7a6bfd855a8433d12504e5aedaf
Pascal <mail@pascal-vogel.com>
2025-03-11 19:50:37 +0100

    Responsive docs index page

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -16,14 +16,14 @@ function HomepageHeader() {
       <div className="absolute z-10 flex w-full flex-col items-center justify-center px-6 text-center text-white">
         <Heading
           as="h1"
-          className="text-7xl font-bold text-gray-800 dark:text-white"
+          className="text-5xl font-bold text-gray-800 md:text-7xl dark:text-white"
         >
           {siteConfig.title}
         </Heading>
-        <p className="mt-4 max-w-3xl text-3xl leading-10 text-gray-800 dark:text-white">
+        <p className="mt-4 max-w-3xl text-xl leading-10 text-gray-800 md:text-3xl dark:text-white">
           {siteConfig.tagline}
         </p>
-        <div className="mt-4 flex flex-wrap items-center justify-center space-x-8 align-middle">
+        <div className="mt-4 flex flex-col items-center justify-center space-y-4 align-middle md:flex-row md:space-y-0 md:space-x-8">
           <Link to="/docs/category/streamlit">
             <button className="border-2 border-lava-600 bg-lava-600 px-8 py-2.5 align-middle text-lg font-semibold text-white hover:cursor-pointer hover:border-lava-700 hover:bg-lava-700 hover:underline">
               Streamlit
