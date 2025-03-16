eba443ce6ef2360bb24fac810457b8b5a9929522
Anton <74004588+antontruly@users.noreply.github.com>
2025-03-16 16:33:34 +0300

    Genie docs

--- streamlit/views/genie_api.py
+++ streamlit/views/genie_api.py
@@ -104,59 +104,59 @@ with tab_a:
 with tab_b:
     st.code(
         """
-        import streamlit as st
-        from databricks.sdk import WorkspaceClient
-        import pandas as pd
+import streamlit as st
+from databricks.sdk import WorkspaceClient
+import pandas as pd
 
-        w = WorkspaceClient()
+w = WorkspaceClient()
 
-        genie_space_id = "01efe16a65e21836acefb797ae6a8fe4"
+genie_space_id = "01f0023d28a71e599b5a62f4117916d4"
 
-        
-        def display_message(message):
-            if "content" in message:
-                st.markdown(message["content"])
-            if "data" in message:
-                st.dataframe(message["data"])
-            if "code" in message:
-                with st.expander("Show generated code"):
-                    st.code(message["code"], language="sql", wrap_lines=True)
 
-                    
-        def get_query_result(statement_id):
-            # For simplicity, let's say data fits in one chunk, query.manifest.total_chunk_count = 1
-
-            result = w.statement_execution.get_statement(statement_id)
-            return pd.DataFrame(
-                result.result.data_array, columns=[i.name for i in result.manifest.schema.columns]
-            )
+def display_message(message):
+    if "content" in message:
+        st.markdown(message["content"])
+    if "data" in message:
+        st.dataframe(message["data"])
+    if "code" in message:
+        with st.expander("Show generated code"):
+            st.code(message["code"], language="sql", wrap_lines=True)
 
             
-        def process_genie_response(response):
-            for i in response.attachments:
-                if i.text:
-                    message = {"role": "assistant", "content": i.text.content}
-                    display_message(message)
-                elif i.query:
-                    data = get_query_result(i.query.statement_id)
-                    message = {
-                        "role": "assistant", "content": i.query.description, "data": data, "code": i.query.query
-                    }
-                    display_message(message)
-                    
+def get_query_result(statement_id):
+    # For simplicity, let's say data fits in one chunk, query.manifest.total_chunk_count = 1
 
-        if prompt := st.chat_input("Ask your question..."):
-            st.chat_message("user").markdown(prompt)
+    result = w.statement_execution.get_statement(statement_id)
+    return pd.DataFrame(
+        result.result.data_array, columns=[i.name for i in result.manifest.schema.columns]
+    )
 
-            with st.chat_message("assistant"):
-                if st.session_state.get("conversation_id"):
-                    conversation = w.genie.create_message_and_wait(
-                        genie_space_id, st.session_state.conversation_id, prompt
-                    )
-                    process_genie_response(conversation)
-                else:
-                    conversation = w.genie.start_conversation_and_wait(genie_space_id, prompt)
-                    process_genie_response(conversation)
+    
+def process_genie_response(response):
+    for i in response.attachments:
+        if i.text:
+            message = {"role": "assistant", "content": i.text.content}
+            display_message(message)
+        elif i.query:
+            data = get_query_result(i.query.statement_id)
+            message = {
+                "role": "assistant", "content": i.query.description, "data": data, "code": i.query.query
+            }
+            display_message(message)
+            
+
+if prompt := st.chat_input("Ask your question..."):
+    st.chat_message("user").markdown(prompt)
+
+    with st.chat_message("assistant"):
+        if st.session_state.get("conversation_id"):
+            conversation = w.genie.create_message_and_wait(
+                genie_space_id, st.session_state.conversation_id, prompt
+            )
+            process_genie_response(conversation)
+        else:
+            conversation = w.genie.start_conversation_and_wait(genie_space_id, prompt)
+            process_genie_response(conversation)
         """
     )
 
