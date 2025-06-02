65bf665235f7af147db30219a1945a65cb4938af
Piero Ocampo <piero.ocampo@gmail.com>
2025-06-02 17:28:05 -0400

    support long ass streaming queries

--- fastapi/app.py
+++ fastapi/app.py
@@ -50,4 +50,4 @@ async def root() -> Dict[str, str]:
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, timeout_keep_alive=1200)
